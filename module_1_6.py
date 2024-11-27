a232494795854b71ed47b2c20baa4a20a7232dd0
DSerega123 <dserega123@gmail.com>
2024-11-27 22:36:59 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,5 @@
+kol_DZ = 12
+kol_chasov = 1.5
+сourse_name = 'Python'
+time = kol_chasov/kol_DZ
+print('Курс:',сourse_name,'всего задач:', kol_DZ, 'затрачено часов:', kol_chasov, 'среднее время выполнения', time, 'часа.')
