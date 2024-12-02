46ad8c61ccca84d6aaedc9545707386f7ac2e2d6
DSerega123 <dserega123@gmail.com>
2024-12-02 21:39:39 +0300

    module_1_6.py

--- module_1_6.py
+++ module_1_6.py
@@ -1,5 +1,14 @@
-kol_DZ = 12
-kol_chasov = 1.5
-сourse_name = 'Python'
-time = kol_chasov/kol_DZ
-print('Курс:',сourse_name,'всего задач:', kol_DZ, 'затрачено часов:', kol_chasov, 'среднее время выполнения', time, 'часа.')
+my_dict = {'Vasya': 1975, 'Egor': 1999, 'Masha': 2002}
+print(my_dict)
+print(my_dict['Masha'])
+print(my_dict.get('Yura'))
+my_dict.update({'Kamila': 1981, 'Artem': 1915})
+a = my_dict.pop('Egor')
+print(a)
+print(my_dict)
+my_set = {'Яблоко', 'Яблоко', 1, 1, 1, 'Яблоко', 'Яблоко', 42.314, 42.314, 42.314, 42.314}
+print(my_set)
+my_set.add(13)
+my_set.update ({(5, 6, 1.6)})
+my_set.remove(1)
+print(my_set)
